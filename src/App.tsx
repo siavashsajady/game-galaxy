0c8ae8e96c2806c6ae9085c4e1cab872e5c086c5
Siavash Sajady <hubgitpro@gmail.com>
2023-03-25 17:46:27 +0000

    feat: build responsive layout
    This commit introduces a responsive layout design that adapts to various screen sizes and devices. Implemented using chakra Grid to ensure a seamless user experience across desktop, tablet, and mobile devices.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area='nav' bg='#D8D8D8'>
+        nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='#E49393'>
+          aside
+        </GridItem>
+      </Show>
+
+      <GridItem area='main' bg='#408E91'>
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
