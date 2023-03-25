001503cff5e4485a10ea2a3f520877ff7fcdc9a8
Siavash Sajady <hubgitpro@gmail.com>
2023-03-25 18:35:53 +0000

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+import logo from '../assets/logo.jpg';
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize='80px' />
+      <Text>Navbar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
