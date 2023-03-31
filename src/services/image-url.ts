9607956600943f02dbfba299be5d37577a87a289
Siavash Sajady <hubgitpro@gmail.com>
2023-03-31 13:29:11 +0100

    Implement SortSelector components to sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return '';
   const target = 'media/';
   const regex = new RegExp(`(${target})`);
   return url.replace(regex, `$1crop/600/400/`);
