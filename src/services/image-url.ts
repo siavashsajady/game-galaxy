0ecb300673c5a4e1071cf0144228e4c3a03e18a5
Siavash Sajady <hubgitpro@gmail.com>
2023-03-31 13:50:08 +0100

    Refactore: handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
   const target = 'media/';
   const regex = new RegExp(`(${target})`);
   return url.replace(regex, `$1crop/600/400/`);
