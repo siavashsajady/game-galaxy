0c85d7dda1021e79da764876740c45a0fda8ca1b
Siavash Sajady <hubgitpro@gmail.com>
2023-04-05 10:25:17 +0100

    Update api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'c7b18323a47d40c394ea5b019646b1f5',
+    key: '0ab02055aecd425e8a08984b4a66646e',
   },
 });
